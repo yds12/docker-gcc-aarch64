54ab74213d9368b58001ba87fe4b42dabf57a52b
yds12 <28710965+yds12@users.noreply.github.com>
2021-02-27 11:59:34 +0100

    Install script.

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Install the compiler in ~/.local/bin
+
+# Default name for the executable
+DEFAULTEXE="compile-aarch64"
+
+# Prompt user for the executable name
+echo "What name do you want for the aarch64-gcc-compiler executable? [$DEFAULTEXE] "
+read EXENAME
+
+# If entered value is empty, uses the default
+[ -z "$EXENAME" ] && EXENAME=$DEFAULTEXE
+
+# Create ~/.local/bin if it doesn't exist yet
+[ ! -d ~/.local/bin ] && mkdir -p ~/.local/bin 
+
+# Copy the compile script there
+cp compile-gcc-aarch64.sh ~/.local/bin
+
+# Create a link with the desirable executable name
+ln -s ~/.local/bin/compile-gcc-aarch64.sh ~/.local/bin/"$EXENAME"
+
